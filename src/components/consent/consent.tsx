f57152848ac29c8317d75d30a3fc1a63049bad80
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2023-01-30 23:25:55 +0100

    Added paused for privacy policy page

--- src/components/consent/consent.tsx
+++ src/components/consent/consent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../styles/index.css'
 import { CookifyProvider } from '../../context/cookifyContext'
 import Detail from './detail/detail'
@@ -6,10 +6,17 @@ import { CookifyConsentProps } from '../../types'
 
 export const Consent: React.FC<CookifyConsentProps> = ({settings, children}) => {
     const {options, consent} = settings
+    const [pageURL, setPageURL] = useState('')
 
     const _this = {
         support: consent?.support ?? true,
         reference: consent?.reference ?? false,
+        paused: {
+            title: consent?.paused?.title || 'Consent Manger Notice',
+            description: consent?.paused?.description || 'The consent manager is paused on this side to read the privacy policy.',
+            icon: consent?.paused?.icon || '⚠️',
+            url: consent?.paused?.url || 'undefined',
+        },
         detail: {
             title: consent?.secound_layer?.title || 'Manage Cookies',
             description: consent?.secound_layer?.description || <>We use cookies to provide and secure our websites, as well as to analyze the usage of our websites, in order to offer you a great user experience. To learn more about our use of cookies see our <a href="#" style={{textDecoration: 'underline', fontWeight: 500}}>Privacy Policy</a>.</>
@@ -21,6 +28,10 @@ export const Consent: React.FC<CookifyConsentProps> = ({settings, children}) => 
         }
     }
 
+    useEffect(() => {
+        setPageURL(window.location.pathname)
+    }, [])
+
     /**
      * Display Info fehlt noch
      * Option um nur Display Info oder nur Detail anzeigen zu lassen
@@ -30,12 +41,26 @@ export const Consent: React.FC<CookifyConsentProps> = ({settings, children}) => 
     return (
         <CookifyProvider options={options}>
             {children}
-            <Detail
-                label={_this.detail}
-                table={_this.table}
-                support={_this.support}
-                reference={_this.reference}
-            />
+
+            {(_this.paused.url !== 'undefined' && _this.paused.url == pageURL) ? (
+                <div className="fixed inset-x-3 sm:inset-x-5 bottom-3 sm:bottom-5 bg-white max-w-sm mr-auto shadow-lg border-2 rounded-lg w-fit">
+                    <div className="px-3 py-2 flex gap-3 items-center">
+                        <div className="text-2xl">{_this.paused.icon}</div>
+                        <div>
+                            <p className="text-xl font-semibold">{_this.paused.title}</p>
+                            <p>{_this.paused.description}</p>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <Detail
+                    label={_this.detail}
+                    table={_this.table}
+                    support={_this.support}
+                    reference={_this.reference}
+                />
+            )}
+            
         </CookifyProvider>
     )
 }
