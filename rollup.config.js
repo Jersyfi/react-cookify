44b78f78a69320d7f9e0f7549275ebd281947f24
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2023-02-13 23:16:41 +0100

    Merge fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
 import terser from "@rollup/plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
 import dts from "rollup-plugin-dts";
@@ -14,6 +15,16 @@ export default [
                 sourcemap: true,
         },
         plugins: [
+            postcss({
+                config: {
+                    path: "./postcss.config.js",
+                },
+                extensions: [".css"],
+                minimize: true,
+                inject: {
+                    insertAt: "top",
+                },
+            }),
             external(),
             resolve(),
             commonjs(),
@@ -24,6 +35,7 @@ export default [
     {
         input: "dist/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts.default()]
+        plugins: [dts.default()],
+        external: [/\.css$/u]
     }
 ]
